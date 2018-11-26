8eb565fae3a3ca03675e598251af01f7920f633b
Rico Huijbers <rix0rrr@gmail.com>
2018-11-26 18:53:11 +0100

    Don't run .js files anymore

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    testRegex: "(/test/.*|(\\.|/)(test|spec))\\.(tsx?)$",
+    testPathIgnorePatterns: ["/node_modules/"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    // collectCoverage: true
 };
